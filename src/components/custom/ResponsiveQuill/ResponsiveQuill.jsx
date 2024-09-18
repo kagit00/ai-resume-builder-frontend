1d0c2e157c6e97302e2e7910988d0b47682d650b
kaustav <kaustavmanna4@gmail.com>
2024-09-18 12:19:54 +0530

    ]]

--- src/components/custom/ResponsiveQuill/ResponsiveQuill.jsx
+++ src/components/custom/ResponsiveQuill/ResponsiveQuill.jsx
@@ -25,8 +25,6 @@ const ResponsiveQuill = ({ value, onChange, placeholder, className, style }) => 
     return (
         <ReactQuill
             ref={quillRef}
-            id={id}
-            name={name}
             value={value}
             onChange={onChange}
             className={`editor-container ${theme}-theme ${className}`}
