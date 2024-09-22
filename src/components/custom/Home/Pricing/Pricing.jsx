9d4ab022110e4073e6959e1e4b6edcaa9026e993
kaustav <kaustavmanna4@gmail.com>
2024-09-22 16:28:34 +0530

    vv

--- src/components/custom/Home/Pricing/Pricing.jsx
+++ src/components/custom/Home/Pricing/Pricing.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Button } from '@/components/ui/button';
 import { ChevronRight } from 'lucide-react';
 
@@ -8,7 +8,9 @@ function Pricing() {
       <div className="container mx-auto px-4 md:px-8 text-center">
         <h3 className="text-4xl md:text-5xl lg:text-6xl py-20 font-thin text-white">Pricing Plans</h3>
         <div className="flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-6">
-          <div className=" p-6 md:p-8 rounded-3xl shadow-2xl w-full max-w-xs">
+          
+          {/* Basic Plan */}
+          <div className="p-6 md:p-8 rounded-3xl shadow-2xl w-full max-w-xs">
             <h4 className="text-xl md:text-2xl font-semibold mb-4 text-gray-100">Basic Plan</h4>
             <p className="text-gray-400 text-lg mb-6">Rs. 0 with Lifetime Access</p>
             <ul className="text-left text-gray-400 mb-6">
@@ -17,21 +19,24 @@ function Pricing() {
               <li className="mb-3">Email support</li>
               <li className="mb-3">Unlimited Resume Edit</li>
             </ul>
-        </div>
+          </div>
+
+          {/* Premium Plan */}
           <div className="bg-gray-800 text-white p-6 md:p-8 rounded-xl shadow-2xl w-full max-w-xs">
             <h4 className="text-xl md:text-2xl font-semibold mb-4">Premium Plan</h4>
             <p className="text-white text-lg mb-6">Rs. 20 with Lifetime Access</p>
             <ul className="text-left mb-6">
-               <li className="mb-3">All Basic Featues</li>
+              <li className="mb-3">All Basic Features</li>
               <li className="mb-3">Analyze Your Resumes & Get Insights</li>
               <li className="mb-3">Download Your Resumes</li>
               <li className="mb-3">Share Resumes on Social</li>
-            </ul>          
-            </div>
+            </ul>
+          </div>
+
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Pricing
+export default Pricing;
